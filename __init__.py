726e8be5e18c90e94a1e11977585645d3f6daf2f
Bryce Goh <brycegohym@gmail.com>
2024-08-06 22:12:16 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,6 +3,7 @@ from .nodes import *
 NODE_CLASS_MAPPINGS = {
     'BlackoutMLSD': BlackoutMLSD,
     'PasteMask': PasteMask,
+    'PrintImageSize': PrintImageSize,
 }
 
 __all__ = ['NODE_CLASS_MAPPINGS']
