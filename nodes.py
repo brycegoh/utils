429a4cc1f87de071a9f41c1dd9cafd74ea665f03
Bryce Goh <brycegohym@gmail.com>
2024-08-12 19:47:33 +0800

    Update nodes.py

--- nodes.py
+++ nodes.py
@@ -5,6 +5,8 @@ import numpy as np
 import torch
 import torchvision.transforms.functional as TF
 import math
+from transformers import OneFormerProcessor, OneFormerForUniversalSegmentation
+import os
 
 def tensor2rgb(t: torch.Tensor) -> torch.Tensor:
     size = t.size()
@@ -181,3 +183,84 @@ class PrintImageSize:
     def func(self, image, prefix):
         print(f"{prefix}: {image.shape}")
         return (image,)
+
+class OneformerDinatSegmentation:
+    CATEGORY = "utils"
+
+    @classmethod    
+    def INPUT_TYPES(cls):
+        return { 
+            "required":{
+                "image": ("IMAGE",),
+            }
+        }
+
+    RETURN_TYPES = ("IMAGE",)
+    RETURN_NAMES = ("image",)
+    FUNCTION = "func"
+
+    def func(self, image):
+        palette = [[120, 120, 120], [180, 120, 120], [6, 230, 230], [80, 50, 50],
+                [4, 200, 3], [120, 120, 80], [140, 140, 140], [204, 5, 255],
+                [230, 230, 230], [4, 250, 7], [224, 5, 255], [235, 255, 7],
+                [150, 5, 61], [120, 120, 70], [8, 255, 51], [255, 6, 82],
+                [143, 255, 140], [204, 255, 4], [255, 51, 7], [204, 70, 3],
+                [0, 102, 200], [61, 230, 250], [255, 6, 51], [11, 102, 255],
+                [255, 7, 71], [255, 9, 224], [9, 7, 230], [220, 220, 220],
+                [255, 9, 92], [112, 9, 255], [8, 255, 214], [7, 255, 224],
+                [255, 184, 6], [10, 255, 71], [255, 41, 10], [7, 255, 255],
+                [224, 255, 8], [102, 8, 255], [255, 61, 6], [255, 194, 7],
+                [255, 122, 8], [0, 255, 20], [255, 8, 41], [255, 5, 153],
+                [6, 51, 255], [235, 12, 255], [160, 150, 20], [0, 163, 255],
+                [140, 140, 140], [250, 10, 15], [20, 255, 0], [31, 255, 0],
+                [255, 31, 0], [255, 224, 0], [153, 255, 0], [0, 0, 255],
+                [255, 71, 0], [0, 235, 255], [0, 173, 255], [31, 0, 255],
+                [11, 200, 200], [255, 82, 0], [0, 255, 245], [0, 61, 255],
+                [0, 255, 112], [0, 255, 133], [255, 0, 0], [255, 163, 0],
+                [255, 102, 0], [194, 255, 0], [0, 143, 255], [51, 255, 0],
+                [0, 82, 255], [0, 255, 41], [0, 255, 173], [10, 0, 255],
+                [173, 255, 0], [0, 255, 153], [255, 92, 0], [255, 0, 255],
+                [255, 0, 245], [255, 0, 102], [255, 173, 0], [255, 0, 20],
+                [255, 184, 184], [0, 31, 255], [0, 255, 61], [0, 71, 255],
+                [255, 0, 204], [0, 255, 194], [0, 255, 82], [0, 10, 255],
+                [0, 112, 255], [51, 0, 255], [0, 194, 255], [0, 122, 255],
+                [0, 255, 163], [255, 153, 0], [0, 255, 10], [255, 112, 0],
+                [143, 255, 0], [82, 0, 255], [163, 255, 0], [255, 235, 0],
+                [8, 184, 170], [133, 0, 255], [0, 255, 92], [184, 0, 255],
+                [255, 0, 31], [0, 184, 255], [0, 214, 255], [255, 0, 112],
+                [92, 255, 0], [0, 224, 255], [112, 224, 255], [70, 184, 160],
+                [163, 0, 255], [153, 0, 255], [71, 255, 0], [255, 0, 163],
+                [255, 204, 0], [255, 0, 143], [0, 255, 235], [133, 255, 0],
+                [255, 0, 235], [245, 0, 255], [255, 0, 122], [255, 245, 0],
+                [10, 190, 212], [214, 255, 0], [0, 204, 255], [20, 0, 255],
+                [255, 255, 0], [0, 153, 255], [0, 41, 255], [0, 255, 204],
+                [41, 0, 255], [41, 255, 0], [173, 0, 255], [0, 245, 255],
+                [71, 0, 255], [122, 0, 255], [0, 255, 184], [0, 92, 255],
+                [184, 255, 0], [0, 133, 255], [255, 214, 0], [25, 194, 194],
+                [102, 255, 0], [92, 0, 255]]
+        model_path = get_model_path()
+        processor = OneFormerProcessor.from_pretrained("shi-labs/oneformer_ade20k_dinat_large", cache_dir=model_path)
+        model = OneFormerForUniversalSegmentation.from_pretrained("shi-labs/oneformer_ade20k_dinat_large", cache_dir=model_path)
+        images = []
+        for i in range(len(image)):
+            img = image[i]
+            if isinstance(img, torch.Tensor):
+                img = 255. * img.cpu().numpy()
+            else:
+                img = 255. * img 
+            img = np.clip(img, 0, 255).astype(np.uint8)
+            img = Image.fromarray(img)
+            
+            semantic_inputs = processor(images=image, task_inputs=["semantic"], return_tensors="pt").to("cuda")
+            with torch.no_grad():
+                semantic_outputs = model(**semantic_inputs)
+            predicted_semantic_map = processor.post_process_semantic_segmentation(semantic_outputs, target_sizes=[image.size[::-1]])[0]
+            predicted_semantic_map_np = predicted_semantic_map.cpu().numpy()
+            palette = np.array(palette)
+            # Map the tensor values to the palette
+            color_image = palette[predicted_semantic_map_np]
+            color_image = torch.from_numpy(color_image / 255.0).unsqueeze(0)  # Add batch dimension
+            images.append(color_image)
+        
+        images = torch.cat(images, dim=0)
+        return (images,)
