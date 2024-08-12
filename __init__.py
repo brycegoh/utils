784419ce4ece66f3092e9e8b19e31e34142a9cca
Bryce Goh <brycegohym@gmail.com>
2024-08-12 19:47:58 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,6 +4,7 @@ NODE_CLASS_MAPPINGS = {
     'BlackoutMLSD': BlackoutMLSD,
     'PasteMask': PasteMask,
     'PrintImageSize': PrintImageSize,
+    'OneformerDinatSeg': OneformerDinatSegmentation,
 }
 
 __all__ = ['NODE_CLASS_MAPPINGS']
